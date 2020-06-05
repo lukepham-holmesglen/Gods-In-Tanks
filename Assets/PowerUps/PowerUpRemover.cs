76e64098c4ffe417052c7c286b92dd91427e5eb6
HuntedGaming <66096435+HuntedGaming@users.noreply.github.com>
2020-06-05 11:45:24 +1000

    Power Up Scripts

---
+++ Assets/PowerUps/PowerUpRemover.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PowerUpRemover : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision collision) //deals with collision
+    {
+        if (collision.gameObject.tag == "Player") //if the player touches the object
+        {
+            Destroy(gameObject); //Destroys the Power Up so it can't be collected twice
+        }
+    }
+}
