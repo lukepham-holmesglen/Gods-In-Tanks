5c9df1791c191f466addfe83992bc63cd7662bca
HuntedGaming <66096435+HuntedGaming@users.noreply.github.com>
2020-06-12 11:40:27 +1000

    Reaching Arms function
    The arms technically function but even though the tank dies it does not end the round

---
+++ Assets/Scripts/Stage Hazards/ReachingArms.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class ReachingArms : MonoBehaviour
+{
+    private GameObject _affectedTank;
+    private MeshRenderer _meshRenderer;
+    private float Timer = 0f;
+
+    void Start()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+        _meshRenderer.enabled = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Timer = 0f;
+    }
+
+    void OnTriggerStay(Collider collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            _affectedTank = collision.gameObject;
+            Timer += Time.deltaTime;
+            if (Timer > 5)
+            {
+                DeathHands();
+            }
+        }
+    }
+
+    void DeathHands()
+    {
+        _affectedTank.GetComponent<TankHealth>().m_CurrentHealth = -10;
+        _affectedTank.GetComponent<TankHealth>().SetHealthUI();
+        _meshRenderer.enabled = true;
+    }
+}
