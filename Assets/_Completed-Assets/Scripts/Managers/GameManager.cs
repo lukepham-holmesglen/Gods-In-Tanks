134103c4f7bcc6e866317b12fc3b0962f55207ff
lukepham-holmesglen <54011967+lukepham-holmesglen@users.noreply.github.com>
2020-06-19 15:33:13 +1000

    Updating Tanks, speed pick ups and bridges to be in the build.

--- Assets/_Completed-Assets/Scripts/Managers/GameManager.cs
+++ Assets/_Completed-Assets/Scripts/Managers/GameManager.cs
@@ -14,7 +14,6 @@ namespace Complete
         public Text m_MessageText;                  // Reference to the overlay Text to display winning text, etc.
         public GameObject m_TankPrefab;             // Reference to the prefab the players will control.
         public TankManager[] m_Tanks;               // A collection of managers for enabling and disabling different aspects of the tanks.
-
         
         private int m_RoundNumber;                  // Which round the game is currently on.
         private WaitForSeconds m_StartWait;         // Used to have a delay whilst the round starts.
--- Assets/_Completed-Assets/Scripts/Managers/TankManager.cs
+++ Assets/_Completed-Assets/Scripts/Managers/TankManager.cs
@@ -6,30 +6,33 @@ namespace Complete
     [Serializable]
     public class TankManager
     {
+
         // This class is to manage various settings on a tank.
         // It works with the GameManager class to control how the tanks behave
         // and whether or not players have control of their tank in the 
         // different phases of the game.
 
         public Color m_PlayerColor;                             // This is the color this tank will be tinted.
+        public GameObject m_PlayerCharacter;
         public Transform m_SpawnPoint;                          // The position and direction the tank will have when it spawns.
+        public Transform m_CharacterSpawnPoint;
         [HideInInspector] public int m_PlayerNumber;            // This specifies which player this the manager for.
         [HideInInspector] public string m_ColoredPlayerText;    // A string that represents the player with their number colored to match their tank.
         [HideInInspector] public GameObject m_Instance;         // A reference to the instance of the tank when it is created.
         [HideInInspector] public int m_Wins;                    // The number of wins this player has so far.
-        
+
 
         private TankMovement m_Movement;                        // Reference to tank's movement script, used to disable and enable control.
         private TankShooting m_Shooting;                        // Reference to tank's shooting script, used to disable and enable control.
         private GameObject m_CanvasGameObject;                  // Used to disable the world space UI during the Starting and Ending phases of each round.
 
 
-        public void Setup ()
+        public void Setup()
         {
             // Get references to the components.
-            m_Movement = m_Instance.GetComponent<TankMovement> ();
-            m_Shooting = m_Instance.GetComponent<TankShooting> ();
-            m_CanvasGameObject = m_Instance.GetComponentInChildren<Canvas> ().gameObject;
+            m_Movement = m_Instance.GetComponent<TankMovement>();
+            m_Shooting = m_Instance.GetComponent<TankShooting>();
+            m_CanvasGameObject = m_Instance.GetComponentInChildren<Canvas>().gameObject;
 
             // Set the player numbers to be consistent across the scripts.
             m_Movement.m_PlayerNumber = m_PlayerNumber;
@@ -39,7 +42,18 @@ namespace Complete
             m_ColoredPlayerText = "<color=#" + ColorUtility.ToHtmlStringRGB(m_PlayerColor) + ">PLAYER " + m_PlayerNumber + "</color>";
 
             // Get all of the renderers of the tank.
-            MeshRenderer[] renderers = m_Instance.GetComponentsInChildren<MeshRenderer> ();
+            MeshRenderer[] renderers = m_Instance.GetComponentsInChildren<MeshRenderer>();
+
+            Transform[] tankObjects = m_Instance.GetComponentsInChildren<Transform>();
+
+            foreach (Transform tankObject in tankObjects)
+            {
+                Debug.Log(tankObject);
+                if (tankObject.name == "CharacterSpawnPoint")
+                {
+                    m_CharacterSpawnPoint = tankObject.transform;
+                }
+            }
 
             // Go through all the renderers...
             for (int i = 0; i < renderers.Length; i++)
@@ -51,33 +65,33 @@ namespace Complete
 
 
         // Used during the phases of the game where the player shouldn't be able to control their tank.
-        public void DisableControl ()
+        public void DisableControl()
         {
             m_Movement.enabled = false;
             m_Shooting.enabled = false;
 
-            m_CanvasGameObject.SetActive (false);
+            m_CanvasGameObject.SetActive(false);
         }
 
 
         // Used during the phases of the game where the player should be able to control their tank.
-        public void EnableControl ()
+        public void EnableControl()
         {
             m_Movement.enabled = true;
             m_Shooting.enabled = true;
 
-            m_CanvasGameObject.SetActive (true);
+            m_CanvasGameObject.SetActive(true);
         }
 
 
         // Used at the start of each round to put the tank into it's default state.
-        public void Reset ()
+        public void Reset()
         {
             m_Instance.transform.position = m_SpawnPoint.position;
             m_Instance.transform.rotation = m_SpawnPoint.rotation;
 
-            m_Instance.SetActive (false);
-            m_Instance.SetActive (true);
+            m_Instance.SetActive(false);
+            m_Instance.SetActive(true);
         }
     }
 }
